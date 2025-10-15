9760ef292de2b8130fb89fa722822435c084ed7f
Valentin-Morette <morette.valentin@gmail.com>
2025-10-15 16:25:45 +0200

    no listen bot

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
-client.on('ready', () => {});
+client.on('ready', () => {
 
-client.on('messageCreate', (message) => {});
+});
+
+client.on('messageCreate', (message) => {
+	if (message.author.bot) return;
+});
 
 client.on('interactionCreate', async (interaction) => {
 	if (interaction.isButton()) {
